fbc5d47ed2c1281a5d28fdf445c1e011fc3545c7
Yu Sheng <ays03@bu.edu>
2026-03-23 21:29:54 -0400

    Update project files

--- Ridership.py
+++ Ridership.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
 
-conn = sqlite3.connect("/Users/davidfeng/Desktop/Edu/BU/4Se26Spring/CS506/finalProj/Ridership.sqlite")
+conn = sqlite3.connect("Ridership copy.sqlite")
 
 query = """
 SELECT stop_name, AVG(average_load) AS avg_load
@@ -14,7 +14,6 @@ ORDER BY avg_load DESC
 
 df = pd.read_sql_query(query, conn)
 
-# Plot the results
 plt.figure(figsize=(10, 6))
 plt.bar(df["stop_name"], df["avg_load"], color="skyblue")
 plt.title("")
